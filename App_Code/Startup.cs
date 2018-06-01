bac4491561adf7aebf219b437935d3facb1d0348
JoaquinMtzP <joaquin_mtz_p@hotmail.com>
2018-05-31 19:00:18 -0500

    Agregar archivos de proyecto.

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetPoliza.Startup))]
+namespace GetPoliza
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
